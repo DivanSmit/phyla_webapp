5a19cd81603d4368794289613d0dc19adced5ff3
Divan Smit <divansmit16@gmail.com>
2023-11-28 09:55:38 +0200

    url

--- src/lib/connect_to_BASE.js
+++ src/lib/connect_to_BASE.js
@@ -1,9 +1,11 @@
+let ngrok = "https://6ec9-102-182-74-10.ngrok.io"+"/erl/http_session_gen_server/fetchdata";
+
 export async function get_data(Tag1, Tag2 = "INFO") {
 
     let MSG = {subject: "INFO", queryParam:Tag2, tag: Tag1};
 
     try {
-        let url = new URL("https://d5d9-102-182-74-10.ngrok.io/erl/http_session_gen_server/fetchdata");
+        let url = new URL(ngrok);
         url.searchParams.append("MSG", JSON.stringify(MSG));
         const response = await fetch(url);
         if (response.ok) {
@@ -24,7 +26,7 @@ export async function spawnInstance(Tag, Param) {
     let MSG = {subject: "SPAWN", queryParam:"SPAWN", tag: Tag, param: Param};
 
     try {
-        let url = new URL("http://localhost:9001/erl/http_session_gen_server/fetchdata");
+        let url = new URL(ngrok);
         url.searchParams.append("MSG", JSON.stringify(MSG));
         const response = await fetch(url);
         if (response.ok) {
@@ -45,7 +47,7 @@ export async function userInteraction(User, Param, ID = '') {
     let MSG = {subject: "USERInteract", user: User, param: Param, tag: "TASKS", taskID: ID};
 
     try {
-        let url = new URL("http://localhost:9001/erl/http_session_gen_server/fetchdata");
+        let url = new URL(ngrok);
         url.searchParams.append("MSG", JSON.stringify(MSG));
         const response = await fetch(url);
         if (response.ok) {
@@ -66,7 +68,7 @@ export async function getuserData(User, Param) {
     let MSG = {subject: "UserData", user: User, param: Param, tag: "INFO"};
 
     try {
-        let url = new URL("http://localhost:9001/erl/http_session_gen_server/fetchdata");
+        let url = new URL(ngrok);
         url.searchParams.append("MSG", JSON.stringify(MSG));
         const response = await fetch(url);
         if (response.ok) {
